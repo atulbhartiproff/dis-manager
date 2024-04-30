892927998b51060f674daa08fea54c9f63d2709e
Atul Bharti <146771106+atulbhartiproff@users.noreply.github.com>
2024-04-30 10:00:36 +0530

    The bare bones of the bot is done

--- code_1.py
+++ code_1.py
@@ -1,11 +1,33 @@
 # This example requires the 'message_content' intent.
 
-import discord
+import discord #Duh, Obviously
+import json #helps in working with json files
+import requests #Does request to API and stuff
+import random #nothing is better than randomness after all. Entropy is the law of universe.
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+#List of Sad words to automatically respond to
+sad_words=["feel sad","am depressed","feel sick","urgh"]
+#Phrases it can respond with
+starter_encouragements = [
+  "Cheer up! You can get through this my guy.",
+  "Hang in there. After all, Duty ends only in death.",
+  "You are a great fella!",
+  "Look around you. Life is bad only if you let it. Let go, take a breathe, AND LETS FUCKING GOOO"
+]
+
+
+#This function requests from the given rl and stores it in the Json file. This it extracts it from the JSon file in a specefic format, isuppose?
+def get_quote():
+    response= requests.get("https://zenquotes.io/api/random")
+    json_data=json.loads(response.text)
+    quote=json_data[0]['q']+"-"+json_data[0]['a']
+    return (quote)
+
 
 @client.event
 async def on_ready():
@@ -15,9 +37,18 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    msg=str.lower(message.content)
 
-    if message.content.startswith('$hello'):
+    if msg.startswith('$inspire'):
+        quote=get_quote()
+        await message.channel.send(quote)
+
+    if msg.startswith('$hello'):
         await message.channel.send('Hello!')
+    
+    if any(word in msg for word in sad_words):
+        await message.channel.send(random.choice(starter_encouragements))
 
 with open('token.txt','r') as file:
     token=file.read()
