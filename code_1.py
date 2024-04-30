087ca9b269a01834c2fa8d93e84c2d45bf1ff3e9
Atul Bharti <146771106+atulbhartiproff@users.noreply.github.com>
2024-04-30 09:16:20 +0530

    Better now

--- code_1.py
+++ code_1.py
@@ -1,7 +1,6 @@
 # This example requires the 'message_content' intent.
 
 import discord
-import sys #This is to import token file
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -20,7 +19,6 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-sys.path.append('C:\Users\harsh\OneDrive\Documents\College Stuff')
-with open('Token for discord bot.txt','r') as file:
+with open('token.txt','r') as file:
     token=file.read()
 client.run(token)
