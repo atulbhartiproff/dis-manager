a0b429eb0e48fc1e86b774bc2a33a30081307930
Atul Bharti <146771106+atulbhartiproff@users.noreply.github.com>
2024-04-30 09:12:57 +0530

    Bot Works Now

--- code_1.py
+++ code_1.py
@@ -1,11 +1,16 @@
-import discord
-import os
+# This example requires the 'message_content' intent.
 
-client=discord.Client()
+import discord
+import sys #This is to import token file
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
@@ -15,4 +20,7 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(os.getenv('TOKEN'))
+sys.path.append('C:\Users\harsh\OneDrive\Documents\College Stuff')
+with open('Token for discord bot.txt','r') as file:
+    token=file.read()
+client.run(token)
